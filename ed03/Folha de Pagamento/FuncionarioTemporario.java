ac8bc5ea785466d246c5d699bd362e16b5ce7560
Matheus Pianaro <jmpianarotec@gmail.com>
2025-05-28 15:06:44 -0300

    Delete ed03/Folha de Pagamento directory

--- ed03/Folha de Pagamento/FuncionarioTemporario.java
+++
@@ -1,9 +0,0 @@
-public class FuncionarioTemporario extends Funcionario{
-    public FuncionarioTemporario(String nome, double salarioBase){
-        super(nome, salarioBase);
-    }
-    @Override
-    public double calcularSalario(){
-        return salarioBase - 100.0;
-    }
-}
